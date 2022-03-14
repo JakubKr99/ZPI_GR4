2526e4d044897a86bc8ec833e59f56d6b437db43
Blizzeq <kubamax4@gmail.com>
2022-03-14 18:35:05 +0100

    implement secondBranch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,1 +1,1 @@
-package com.company;public class Main {    public static void main(String[] args) {        System.out.println("Hello world!");    }}
+package com.company;public class Main {    public static void main(String[] args) {        System.out.println("Hello world!");        System.out.println("Linia secondBranch!");    }}
